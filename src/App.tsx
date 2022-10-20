93185d4b4787b925a5e0c90ab900c018634006a1
David Figueroa <david@alignable.com>
2022-10-20 18:23:06 -0500

    layout - footer added - github link

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,15 @@ import Home from "./pages/Home";
 
 const App: Component = () => {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+    <div class="flex flex-col h-screen">
+      <main class="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+      <footer class="text-center py-2">
+        <a href="https://github.com/dmfigueroa/easy-brackets" target="_blank">GitHub</a>
+      </footer>
     </div>
   );
 };
